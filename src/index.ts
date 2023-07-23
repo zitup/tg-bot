4dfa650027d402c602108fc3b8138c2b7538736e
zitup <yitongzhao@163.com>
2023-07-23 11:10:08 +0800

    Requirements 2

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,13 @@
-import { Bot, InlineKeyboard } from "grammy";
+import { Bot } from "grammy";
+import { generateKeyboard } from "./keyboard";
 
 const bot = new Bot('');
 
-const keyboard = new InlineKeyboard()
-  .text('Add', 'add')
-  .text('Switch', 'switch')
-  .row()
-  .text('Buy 0.01', 'buy_0_01')
-  .text('Buy 0.05', 'buy_0_05');
-
+const keyboard = generateKeyboard(bot);
 bot.command('start', ctx => {
   ctx.reply('Hello, I am your bot. What would you like to do?', {
       reply_markup: keyboard
   
-
-
-bot.callbackQuery('add', async ctx => {
-  await ctx.answerCallbackQuery();
-  await ctx.reply('You clicked Add!');
-
-
-bot.callbackQuery('switch', async ctx => {
-  await ctx.answerCallbackQuery();
-  await ctx.reply('You clicked Switch!');
-
-
-bot.callbackQuery('buy_0_01', async ctx => {
-  await ctx.answerCallbackQuery();
-  await ctx.reply('You clicked Buy 0.01!');
-
-
-bot.callbackQuery('buy_0_05', async ctx => {
-  await ctx.answerCallbackQuery();
-  await ctx.reply('You clicked Buy 0.05!');
 
 
 bot.start();
---
+++ src/keyboard.ts
@@ -0,0 +1,47 @@
+import { Bot, InlineKeyboard } from "grammy";
+
+let isBuy = true;
+
+export const generateKeyboard = (bot: Bot) => {
+  const defaultKeyboard = new InlineKeyboard()
+    .text('Add', 'add')
+    .text('Buy ↔️ Sell', 'switch')
+    .row()
+    .text('Buy 0.01', 'action_0_01')
+    .text('Buy 0.05', 'action_0_05');
+
+  bot.callbackQuery('add', async ctx => {
+    await ctx.answerCallbackQuery();
+    await ctx.reply('You clicked Add!');
+  
+  
+  bot.callbackQuery('switch', async ctx => {
+    await ctx.answerCallbackQuery();
+
+    // 更新状态变量
+    isBuy = !isBuy;
+
+    // 更新inline键盘
+    const newKeyboard = new InlineKeyboard()
+        .text('Add', 'add')
+        .text('Buy ↔️ Sell', 'switch')
+        .row()
+        .text(isBuy ? 'Buy 0.01' : 'Sell 0.01', 'action_0_01')
+        .text(isBuy ? 'Buy 0.05' : 'Sell 0.05', 'action_0_05');
+    await ctx.editMessageReplyMarkup({
+        reply_markup: newKeyboard
+    
+  
+
+  bot.callbackQuery('action_0_01', async ctx => {
+      await ctx.answerCallbackQuery();
+      await ctx.reply(`You clicked ${isBuy ? 'Buy' : 'Sell'} 0.01!`);
+  
+
+  bot.callbackQuery('action_0_05', async ctx => {
+      await ctx.answerCallbackQuery();
+      await ctx.reply(`You clicked ${isBuy ? 'Buy' : 'Sell'} 0.05!`);
+  
+
+  return defaultKeyboard;
+}
